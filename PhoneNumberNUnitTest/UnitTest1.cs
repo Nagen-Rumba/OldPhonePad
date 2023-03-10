1108dbe2b5b4cc9b1b997aac736ad8ed0cfe2f57
Nagen Rumba <nagenrumba9@gmail.com>
2023-03-10 23:44:43 +0700

    Additional Test cases added/ Program logic fixed

--- PhoneNumberNUnitTest/UnitTest1.cs
+++ PhoneNumberNUnitTest/UnitTest1.cs
@@ -1,8 +1,4 @@
 using NUnit.Framework;
-<<<<<<< HEAD
-=======
-using System;
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
 namespace PhoneNumberNUnitTest
 {
@@ -20,7 +16,6 @@ namespace PhoneNumberNUnitTest
             Assert.AreEqual(expectedOutput, actualOutput);
         }
         [Test]
-<<<<<<< HEAD
         public void TestInput_ManyAsterisk()
         {
             string expectedOutput = "";
@@ -32,12 +27,6 @@ namespace PhoneNumberNUnitTest
         {
             // Arrange
             string input = "3#";
-=======
-        public void CheckSequence_InputWithOneDigit_ReturnsExpectedResult()
-        {
-            // Arrange
-            string input = "3";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
             string expected = "D";
 
             // Act
@@ -51,13 +40,8 @@ namespace PhoneNumberNUnitTest
         public void CheckSequence_InputWithMultipleDigits_ReturnsExpectedResult()
         {
             // Arrange
-<<<<<<< HEAD
             string input = "227#";
             string expected = "BP";
-=======
-            string input = "227";
-            string expected = "B";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
             // Act
             string result = PhoneNumber.checkSequence(input);
@@ -70,13 +54,8 @@ namespace PhoneNumberNUnitTest
         public void CheckSequence_InputWithRepeatedDigits_ReturnsExpectedResult()
         {
             // Arrange
-<<<<<<< HEAD
             string input = "4443#";
             string expected = "ID";
-=======
-            string input = "4443";
-            string expected = "I";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
             // Act
             string result = PhoneNumber.checkSequence(input);
@@ -90,11 +69,7 @@ namespace PhoneNumberNUnitTest
         {
             // Arrange
             string input = "22*2#";
-<<<<<<< HEAD
             string expected = "A";
-=======
-            string expected = "B";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
             // Act
             string result = PhoneNumber.checkSequence(input);
@@ -108,11 +83,7 @@ namespace PhoneNumberNUnitTest
         {
             // Arrange
             string input = "11* 222#";
-<<<<<<< HEAD
             string expected = "C";
-=======
-            string expected = "'A C";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
             // Act
             string result = PhoneNumber.checkSequence(input);
@@ -125,7 +96,6 @@ namespace PhoneNumberNUnitTest
         public void CheckSequence_InputWithInvalidCharacters_ThrowsException()
         {
             // Arrange
-<<<<<<< HEAD
             string input = "12X#";
 
             string expected = "&A";
@@ -135,23 +105,13 @@ namespace PhoneNumberNUnitTest
 
             // Assert
             Assert.AreEqual(expected, result);
-=======
-            string input = "12X";
-
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => PhoneNumber.checkSequence(input));
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
         }
 
         [Test]
         public void CheckSequence_InputWithLengthLessThan2_ReturnsEmptyString()
         {
             // Arrange
-<<<<<<< HEAD
             string input = "#";
-=======
-            string input = "1";
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
 
             // Act
             string result = PhoneNumber.checkSequence(input);
@@ -161,7 +121,6 @@ namespace PhoneNumberNUnitTest
         }
 
         [Test]
-<<<<<<< HEAD
         public void CheckSequence_InputWithLengthLessThan1_ReturnsEmptyString()
         {
             // Arrange
@@ -172,15 +131,6 @@ namespace PhoneNumberNUnitTest
 
             // Assert
             Assert.IsEmpty(result);
-=======
-        public void CheckSequence_InputWithLengthMoreThan20_ThrowsException()
-        {
-            // Arrange
-            string input = "12345678901234567890";
-
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => PhoneNumber.checkSequence(input));
->>>>>>> 0a63710de8bef26935e07d14e1ec6ba0bcead9b9
         }
     }
 }
